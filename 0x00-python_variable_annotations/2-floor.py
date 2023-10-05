c1d86e2f963b88a74902efa56930d19b461b3c54
SKGakiria <swgakiria@gmail.com>
2023-10-05 03:02:21 -0700

    Added Task 2 - Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module containing function to calculate the floor value of a float"""
+import math
+
+
+def floor(n: float) -> int:
+    """Function returns the floor of a float"""
+    return math.floor(n)
