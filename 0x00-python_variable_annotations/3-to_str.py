e2120815f8483c331f671e0d8be44963399e4f48
SKGakiria <swgakiria@gmail.com>
2023-10-05 03:12:24 -0700

    Added Task 3 - Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module containing function to convert float to a string representation"""
+
+
+def to_str(n: float) -> str:
+    """Function returns string representation of a float"""
+    return str(n)
