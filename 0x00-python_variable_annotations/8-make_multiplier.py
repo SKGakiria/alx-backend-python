4edfa95946f061a4a2a5c3e233666d80c08f7083
SKGakiria <swgakiria@gmail.com>
2023-10-05 05:38:09 -0700

    Added Task 8 - Complex types - functions

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Module containing function that multiplies a float by a multipier"""
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Function returns a function that multiplies a float by multiplier"""
+    def multiplier_funct(num: float) -> float:
+        """Function multiplies a float by multiplier"""
+        return num * multiplier
+
+    return multiplier_func
