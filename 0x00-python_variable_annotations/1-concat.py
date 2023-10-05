5e4db4a5ac0d4b7ba5b07e61858a3fe6e0178f1a
SKGakiria <swgakiria@gmail.com>
2023-10-05 02:54:28 -0700

    Added Task 1 - Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module containing function concatenating two strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Function that returns a concatenated string"""
+    return str1 + str2
