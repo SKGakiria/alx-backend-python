6d6cbca48bca416cfd87e54dc5d4f68ba22ccb29
SKGakiria <swgakiria@gmail.com>
2023-10-09 10:36:34 -0700

    Added Task 1 - Async Comprehensions

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module containing function with corouting that collect 10 random numbers"""
+from typing import List
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Function returns 10 random numbers"""
+    return [num async for num in async_generator()]
