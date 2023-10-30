ec9ca959e95c126af29789e324a62cb8d47b72ff
SKGakiria <swgakiria@gmail.com>
2023-10-30 06:37:12 +0000

    Added README.md file

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+"""Module contains tests for the utils.access_nested_map"""
+import unittest
+import requests
+from unittest.mock import patch
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+from typing import Mapping, Sequence, Any
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """TestAccessNestedMap class that inherits from unittest.TestCase"""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence, expected: int) -> None:
+        """Method tests the access_nested_map"""
+        result = access_nested_map(nested_map, path)
+        self.assertEqual(result, expected)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b"))
+    ])
+
+    def test_access_nested_map_exception(self, nested_map: Mapping,
+                                         path: Sequence) -> None:
+        """Method raises an error when expected to during testing"""
+        with self.assertRaises(Exception):
+            access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """TestGetJson class that inherits from unittest.TestCase"""
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
+    ])
+
+    @patch("requests.get")
+
+    def test_get_json(self, test_url, test_payload, mock_requests_get):
+        """Method tests the get_json"""
+        mock_requests_get.return_value.json.return_value = test_payload
+        response = get_json(test_url)
+        self.assertEqual(response, test_payload)
+        mock_requests_get.assert_called_once_with(test_url)
+
+
+class TestMemoize(unittest.TestCase):
+    """TestMemoize class that inherits from unittest.TestCase"""
+    def test_memoize(self):
+        """Method tests that utils.memoize decorator works as intended"""
+        class TestClass:
+            """Represents the TestClass"""
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with patch.object(TestClass, 'a_method') as mock_object:
+            test = TestClass()
+            test.a_property()
+            test.a_property()
+            mock_object.assert_called_once()
